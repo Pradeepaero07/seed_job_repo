5534f548dd3a2a2524287c6a44857d911cf4a577
Pradeep Kumar <PKumar1@mindtree_odc01.com>
2018-09-05 18:45:41 +0530

    initial commit

---
+++ first_seedjob.groovy
@@ -0,0 +1,22 @@
+import hudson.model.*
+
+
+Build build = Executor.currentExecutor().currentExecutable
+ParametersAction parametersAction = build.getAction(ParametersAction)
+parametersAction.parameters.each { ParameterValue v ->
+    println v
+}
+
+def hardcoded_param = "Project_Name"
+def resolver = build.buildVariableResolver
+def hardcoded_param_value = resolver.resolve(hardcoded_param)
+
+
+pipelineJob(hardcoded_param_value) {
+    scm {
+        git('https://github.com/Pradeepaero07/mvndemo')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
