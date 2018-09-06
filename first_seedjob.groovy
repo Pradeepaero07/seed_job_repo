7fe97dae12860a17a5d00a736cb90b00da12bb92
Pradeepaero07 <pradeepaero07@gmail.com>
2018-09-06 15:08:19 +0530

    updated the file

--- first_seedjob.groovy
+++ first_seedjob.groovy
@@ -20,7 +20,8 @@ pipelineJob(hardcoded_param_value) {
             script("""
                 node{
 			stage('Checkout'){
-				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Pradeepaero07/mvndemo.git']]])
+				//checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Pradeepaero07/mvndemo.git']]])
+				git 'https://github.com/Pradeepaero07/mvndemo.git'
 			}
 
 			stage('Testing Stage'){
